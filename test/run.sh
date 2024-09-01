4c0bbb445db04aed4c6899fc3d8040222677d905
mojyack <mojyack@gmail.com>
2024-09-01 19:23:07 +0900

    fix test

--- test/run.sh
+++ test/run.sh
@@ -1,13 +1,12 @@
 #!/bin/zsh
 set -e
 
-workdir="$PWD/test"
-rootfs="$workdir/mnt"
+rootfs="mnt"
 
 mkdir $rootfs/{e1d,e2d,e3d}
-echo "$workdir/example1d.sh" > $rootfs/e1d/args
-echo "$workdir/example2d.sh" > $rootfs/e2d/args
-echo "$workdir/example3d.sh" > $rootfs/e3d/args
+echo "$PWD/example1d.sh" > $rootfs/e1d/args
+echo "$PWD/example2d.sh" > $rootfs/e2d/args
+echo "$PWD/example3d.sh" > $rootfs/e3d/args
 truncate -s 4096 $rootfs/e1d/stdout
 truncate -s 4096 $rootfs/e1d/stderr
 echo up > $rootfs/e1d/state
